80f97eaf271696ee96b87a711a8d47099f03f807
Eric Biewener <eric.biewener0@walmart.com>
2022-05-25 16:25:57 -0700

    more - 4:25 pm - May 25

--- src/foods.ts
+++ src/foods.ts
@@ -3,71 +3,86 @@ export type Food = {
   protein?: boolean;
   carbs?: boolean;
   veggies?: boolean;
-  weight: number;
   sides?: Food[];
 };
 
 type FoodWithoutName = Omit<Food, "name">;
 
-const rice: Food = { name: "rice", carbs: true, weight: 100 };
-const polenta: Food = { name: "polenta", carbs: true, weight: 0 };
+const rice: Food = { name: "rice", carbs: true };
+const bread: Food = { name: "crusty bread", carbs: true };
+const polenta: Food = { name: "polenta", carbs: true };
 
 const shrimp: FoodWithoutName = {
   protein: true,
-  weight: 75,
   sides: [rice],
 };
 
 const veggies: Food[] = [
-  { name: "salad", veggies: true, weight: 100 },
-  { name: "broccoli", veggies: true, weight: 50 },
-  { name: "green beans", veggies: true, weight: 50 },
-  { name: "asparagus", veggies: true, weight: 75 },
-  { name: "wilted chard", veggies: true, weight: 10 },
-  { name: "wilted kale", veggies: true, weight: 10 },
-  { name: "wilted spinach", veggies: true, weight: 10 },
+  { name: "salad", veggies: true },
+  { name: "broccoli", veggies: true },
+  { name: "green beans", veggies: true },
+  { name: "asparagus", veggies: true },
+  { name: "wilted chard", veggies: true },
+  { name: "wilted kale", veggies: true },
+  { name: "wilted spinach", veggies: true },
+  { name: "chopped salad with feta, lime, & mint", veggies: true },
 ];
 
-const carbs: Food[] = [rice, { name: "crusty bread", carbs: true, weight: 10 }];
+const carbs: Food[] = [rice, bread];
 
 const proteins: Food[] = [
-  { name: "rotisserie chicken", protein: true, weight: 100 },
-  { name: "salmon", protein: true, weight: 100 },
-  { name: "steak", protein: true, weight: 50 },
+  { name: "rotisserie chicken", protein: true },
+  { name: "salmon", protein: true },
+  { name: "steak", protein: true },
   { name: "garlic shrimp", ...shrimp },
   { name: "anne red sauce shrimp", ...shrimp },
-  { name: "lentils", protein: true, weight: 50 },
-  { name: "pesto turkey burgers", protein: true, weight: 100 },
-  { name: "chicken", protein: true, weight: 100 },
-  { name: "new seasons chicken kebab", protein: true, weight: 100 },
+  { name: "lentils", protein: true },
+  { name: "roasted sausages with grapes & onions", protein: true },
+  { name: "pesto turkey burgers", protein: true },
+  { name: "chicken", protein: true },
+  { name: "new seasons chicken kebab", protein: true },
+  { name: "shakshuka, maybe with sausage", protein: true },
+  { name: "chicken & rice soup with garlicky chile oil", protein: true },
+  { name: "lentisl casero", protein: true },
+  {
+    name: "easy ground chicken noodle soup",
+    protein: true,
+    veggies: true,
+    sides: [bread],
+  },
 ];
 
 const combos: Food[] = [
-  { name: "fajita bowl", protein: true, veggies: true, carbs: true, weight: 100 },
-  { name: "sheet pan greek chicken", protein: true, veggies: true, weight: 50 },
-  { name: "crispy tortellini", protein: true, carbs: true, weight: 50 },
+  { name: "fajita bowl", protein: true, veggies: true, carbs: true },
+  { name: "sheet pan greek chicken", protein: true, veggies: true },
+  { name: "crispy tortellini", protein: true, carbs: true },
+  { name: "chicken gyro salad", protein: true, carbs: true, veggies: true },
+  { name: "carnitas tacos", protein: true, carbs: true, veggies: true },
+  { name: "martha stewart one-pan pasta", protein: true, carbs: true },
   {
-    name: "spicy squash salad with lentils and goat cheese",
+    name: "creamy pappardelle with leeks & bacon",
+    protein: true,
+    carbs: true,
+  },
+  {
+    name: "spicy squash salad with lentils & goat cheese",
     protein: true,
     carbs: true,
     veggies: true,
-    weight: 50,
   },
   {
     name: "easiest chicken noodle soup",
     protein: true,
     veggies: true,
     carbs: true,
-    weight: 50,
   },
   {
-    name: "oven braised beef with tomato and garlic",
+    name: "oven braised beef with tomato & garlic",
     protein: true,
     carbs: true,
-    weight: 50,
     sides: [polenta],
   },
-  { name: "white chili", protein: true, carbs: true, weight: 50 },
+  { name: "white chili", protein: true, carbs: true },
 ];
 
 export const foods = [...veggies, ...carbs, ...proteins, ...combos];
